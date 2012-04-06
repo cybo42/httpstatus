25c0c4ae38464c0c6cfca0ddec64b2971bbadebc
Joe Cyboski <joseph_cyboski@condenast.com>
2012-04-06 11:11:49 -0400

    casting code param to int

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express.createServer();
 
 app.get('/code/:code', function(req, res){
-    var code = req.params.code;
+    var code = parseInt(req.params.code);
     console.log("Responding with HTTP Status code: " + code);
-    res.send("HTTP Status code: " + code, parseInt(code));
+    res.send("HTTP Status code: " + code, code);
 });
 
 var port = process.env.PORT || 8888;
