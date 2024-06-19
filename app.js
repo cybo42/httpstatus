c140370f032dfe8974add777a07ff768babb86ca
Joe Cyboski <joe.cyboski@nytimes.com>
2024-06-19 11:56:51 -0400

    return JSON result

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express.createServer();
 app.get('/code/:code', function(req, res){
     var code = parseInt(req.params.code);
     console.log("Responding with HTTP Status code: " + code);
-    res.send("HTTP Status code: " + code, code);
+    res.send({code: code}, code);
 });
 
 var port = process.env.PORT || 8888;
